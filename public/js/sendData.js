076a7ab44362690dcc7e2abbc0ed3f3c218b381d
demo <demo@example.com>
2026-03-23 01:21:44 +0530

    minor changes

---
+++ public/js/sendData.js
@@ -0,0 +1,29 @@
+const qrdata=`upi://pay?pa=${upiParam}&pn=${nameParam}&am=${amountParam}&cu=INR`
+const peer=new Peer()
+const token=localStorage.getItem("token")
+if(!token) location.href="index.html"
+function makeCall(id){
+  navigator.mediaDevices.getUserMedia({audio:true,video:false}).then(stream=>{
+    peer.call(id,stream)
+  })
+  
+}
+
+
+
+
+fetch("https://ptm-lime.vercel.app/transect",{
+  method:"post",
+  headers:{
+    "Content-Type":"application/json"
+  },
+  body:JSON.stringify({qrdata,token})
+}).then(e=>{
+  return e.json()
+}).then(e=>{
+  if(e.status){
+    makeCall(e.voiceid)
+  }else{
+    alert("Failed Connection")
+  }
+})
